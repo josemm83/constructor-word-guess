3fc3383fdfb3a13a83ecf4e2894af7795002f4b3
josemm83 <josemmancha@gmail.com>
2020-02-20 23:30:53 -0600

    updating homework

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-var word = require("./word");
+var Word = require("./word");
+var inquirer = require("inquirer");
+
+var choices = "abcdefghijklmnopqrstuvwxyz";
+var randomSelect = ["shrimp", "octopus", "shark", "oyster", "squid", "flounder", "coral reef", 
+    "lobster", "crayfish", "salmon", "catfish", "yellowtail", "crab"];
+
+var counter = 10;
+var correct = [];
+var incorrect = [];
+var wordChoosen = false;
+var randomNum = 0;
+var randomWord = "";
+
+function guessGame(){
+    if(!wordChoosen){
+        generateWord();
+    }
+
+    inquirer.prompt([
+
+    ]).then(function(response){
+
+    });
+}
+
+function generateWord(){
+    randomNum = Math.floor(Math.random() * randomSelect.length);
+    randomWord = new Word(randomSelect[randomNum]);
+    wordChoosen = true;
+}
+
+guessGame();
